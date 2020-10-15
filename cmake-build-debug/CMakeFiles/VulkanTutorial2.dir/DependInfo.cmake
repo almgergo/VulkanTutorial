bebb1c3b1d20c574caab6d5e0f13c0fd5c79fbd8
Gergo Almasy <gergo.almasy@chappuishalder.com>
2020-10-15 21:56:58 +0200

    Vulkan test init

---
+++ cmake-build-debug/CMakeFiles/VulkanTutorial2.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/Projects/C++/VulkanTutorial2/src/main.cpp" "E:/Projects/C++/VulkanTutorial2/cmake-build-debug/CMakeFiles/VulkanTutorial2.dir/src/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/Software/glfw-3.3.2.bin.WIN64/include"
+  "C:/Software/glm-0.9.9.8"
+  "C:/VulkanSDK/1.2.154.1/Include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/VulkanTutorial2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanTutorial2.dir/src/main.cpp.obj"
+  "VulkanTutorial2.exe"
+  "VulkanTutorial2.exe.manifest"
+  "VulkanTutorial2.lib"
+  "VulkanTutorial2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanTutorial2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
